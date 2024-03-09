c14f84fad26f1b989b686339d269ebfd4b6d3659
Felipe Beskow <felipebeskow@outlook.com>
2024-03-09 03:38:50 -0300

    Tentiva com o Flutter BluePLUS, porém foi a mesma coisa

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 // import 'dart:io';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_blue/flutter_blue.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:permission_handler_platform_interface/permission_handler_platform_interface.dart';
 
@@ -65,31 +67,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final FlutterBlue _flutterBlue = FlutterBlue.instance;
   var _permissionStatus;
   final PermissionHandlerPlatform _permissionHandler =
       PermissionHandlerPlatform.instance;
   var _ble = [];
 
   @override
-  void initState() {
+  initState() {
     super.initState();
     requestPermission(Permission.bluetoothConnect);
     requestPermission(Permission.bluetoothScan);
-
-    //Start scanning
-    _flutterBlue.startScan(timeout: const Duration(seconds: 4));
-
-    // // Listen to scan results
-    _flutterBlue.scanResults.listen((results) {
-      // do something with scan results
-      for (ScanResult r in results) {
-        print('${r.device} found! rssi: ${r.rssi}');
-      }
-    });
-
-    // Stop scanning
-    // _flutterBlue.stopScan();
+    initFlutterBlue();
   }
 
   Future<void> requestPermission(Permission permission) async {
@@ -99,6 +87,82 @@ class _MyHomePageState extends State<MyHomePage> {
       _permissionStatus = status[permission] ?? PermissionStatus.denied;
       print(_permissionStatus);
     });
+  }
+
+  Future<void> initFlutterBlue() async {
+    // first, check if bluetooth is supported by your hardware
+    // Note: The platform is initialized on the first call to any FlutterBluePlus method.
+    if (await FlutterBluePlus.isSupported == false) {
+      print("Bluetooth not supported by this device");
+      return;
+    }
+
+    // handle bluetooth on & off
+    // note: for iOS the initial state is typically BluetoothAdapterState.unknown
+    // note: if you have permissions issues you will get stuck at BluetoothAdapterState.unauthorized
+    var subscription =
+        FlutterBluePlus.adapterState.listen((BluetoothAdapterState state) {
+      print(state);
+      if (state == BluetoothAdapterState.on) {
+        // usually start scanning, connecting, etc
+
+        // listen to scan results
+        // Note: `onScanResults` only returns live scan results, i.e. during scanning
+        // Use: `scanResults` if you want live scan results *or* the results from a previous scan
+        var subscriptionInner = FlutterBluePlus.onScanResults.listen(
+          (results) {
+            if (results.isNotEmpty) {
+              results.forEach((r) {
+                print(
+                    '${r.device.remoteId}: "${r.advertisementData.advName}" found!');
+              }); // the most recently found device
+            }
+          },
+          onError: (e) => print(e),
+        );
+
+        // cleanup: cancel subscription when scanning stops
+        // FlutterBluePlus.cancelWhenScanComplete(subscriptionInner);
+
+        // Wait for Bluetooth enabled & permission granted
+        // In your real app you should use `FlutterBluePlus.adapterState.listen` to handle all states
+        FlutterBluePlus.adapterState
+            .where((val) => val == BluetoothAdapterState.on)
+            .first;
+
+        // Start scanning w/ timeout
+        // Optional: you can use `stopScan()` as an alternative to using a timeout
+        // Note: scan filters use an *or* behavior. i.e. if you set `withServices` & `withNames`
+        //   we return all the advertisments that match any of the specified services *or* any
+        //   of the specified names.
+        FlutterBluePlus.startScan(
+            // withServices: [Guid("180D")],
+            //withNames: ["Holy-IOT"],
+            timeout: Duration(seconds: 150));
+
+        // wait for scanning to stop
+        FlutterBluePlus.isScanning.where((val) => val == false).first;
+      } else {
+        // show an error to the user, etc
+        const errorSnack = SnackBar(
+          content: Text('Veja seu bluetooth!'),
+        );
+        print('Erro ao ativar bluetooth!');
+
+        // Find the ScaffoldMessenger in the widget tree
+        // and use it to show a SnackBar.
+        ScaffoldMessenger.of(context).showSnackBar(errorSnack);
+      }
+    });
+
+    // turn on bluetooth ourself if we can
+    // for iOS, the user controls bluetooth enable/disable
+    if (Platform.isAndroid) {
+      await FlutterBluePlus.turnOn();
+    }
+
+    // cancel to prevent duplicate listeners
+    // subscription.cancel();
   }
 
   // int _counter = 0;
